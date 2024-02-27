45e720665a6cac1a22da22c52f9d08c7cfe02a26
PriyaDarshan N <96416309+PriyaDarshanN@users.noreply.github.com>
2024-02-27 09:08:54 +0000

    type conversion

---
+++ 01_basics/03_conversionOper.js
@@ -0,0 +1,20 @@
+// let score = false
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log( valueInNumber);
+
+
+// "44"=> 44
+// "44abe" => NaN
+
+let isLoggedIn = 1 
+let BooleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(BooleanIsLoggedIn);
+
+// 1 => true // 0 = > false 
+// ""=> false
+// "priya" => true
