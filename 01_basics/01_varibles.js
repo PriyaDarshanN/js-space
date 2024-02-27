dbe1f4442b2364044d81126757ff92d98432b2be
PriyaDarshan N <96416309+PriyaDarshanN@users.noreply.github.com>
2024-02-27 08:52:01 +0000

    datatypes

---
+++ 01_basics/01_varibles.js
@@ -0,0 +1,22 @@
+const accountId = 13345
+let accountEmail = "priya@gmail.com"
+var accountPassword = "12345"
+accountCity = "Bangalore"
+let accountState;
+console.table([accountEmail,accountId,accountPassword,accountCity])
+// accountId = 2 // cant change const
+
+accountEmail = "pri@yo.com"
+accountPassword = "121212"
+accountCity = "laggere"
+
+console.log(accountId);
+
+/*
+use let 
+dont use var for varibales
+*/
+
+
+
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3 ) // we are using nodeJS not brower
+
+console.log(3 + 3 )
+
+console.log("Priya") // code reable **
+
+let name = "priya"
+let age = 20
+let isLoggedIn = false
+
+
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
